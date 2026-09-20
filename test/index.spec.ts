ceca2a778c6463cf149d9061d6fb1a90e8700d8b
Angel751658 <angelmanzo@gmail.com>
2026-09-19 20:04:05 -0600

    deploy 2

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -19,19 +19,11 @@ describe("Hello World worker", () => {
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"
-			╱|、
-			(˚ˎ 。7
-			|、˜〵
-			じしˍ,)ノ"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"HOLA ;)"`);
 	});
 
 	it("responds with Hello World! (integration style)", async () => {
 		const response = await SELF.fetch("https://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"
-			╱|、
-			(˚ˎ 。7
-			|、˜〵
-			じしˍ,)ノ"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"HOLA ;)"`);
 	});
 });
