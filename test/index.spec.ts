f298519e4e6050f59ab7f658a53595355bdf2a90
Angel751658 <angelmanzo@gmail.com>
2026-09-19 19:59:59 -0600

    deploy 2

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -19,11 +19,19 @@ describe("Hello World worker", () => {
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"
+			╱|、
+			(˚ˎ 。7
+			|、˜〵
+			じしˍ,)ノ"`);
 	});
 
 	it("responds with Hello World! (integration style)", async () => {
 		const response = await SELF.fetch("https://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"
+			╱|、
+			(˚ˎ 。7
+			|、˜〵
+			じしˍ,)ノ"`);
 	});
 });
