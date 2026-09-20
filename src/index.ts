10fa4a69c5ccd11346f12cf558825a592551a3a2
Angel751658 <angelmanzo@gmail.com>
2026-09-19 20:10:51 -0600

    deploy

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("HOLA ;)");
+		return new Response("Hello World!");
 	},
 } satisfies ExportedHandler<Env>;
